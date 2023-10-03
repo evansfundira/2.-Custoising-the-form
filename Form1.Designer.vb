c7d037fa37c51fc87c194e817c1b49a1fec3c47b
evansfundira <evansfundira@gmail.com>
2023-10-03 10:49:14 +0200

    Add project files.

---
+++ Form1.Designer.vb
@@ -0,0 +1,61 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        btnClose = New Button()
+        btnEvansFundra = New Button()
+        SuspendLayout()
+        ' 
+        ' btnClose
+        ' 
+        btnClose.Location = New Point(382, 263)
+        btnClose.Name = "btnClose"
+        btnClose.Size = New Size(94, 29)
+        btnClose.TabIndex = 0
+        btnClose.Text = "Close"
+        btnClose.UseVisualStyleBackColor = True
+        ' 
+        ' btnEvansFundra
+        ' 
+        btnEvansFundra.Location = New Point(299, 111)
+        btnEvansFundra.Name = "btnEvansFundra"
+        btnEvansFundra.Size = New Size(177, 70)
+        btnEvansFundra.TabIndex = 1
+        btnEvansFundra.Text = "Evans Fundira"
+        btnEvansFundra.UseVisualStyleBackColor = True
+        ' 
+        ' Form1
+        ' 
+        AutoScaleDimensions = New SizeF(8F, 20F)
+        AutoScaleMode = AutoScaleMode.Font
+        ClientSize = New Size(800, 450)
+        Controls.Add(btnEvansFundra)
+        Controls.Add(btnClose)
+        Name = "Form1"
+        Text = "Form1"
+        ResumeLayout(False)
+    End Sub
+
+    Friend WithEvents btnClose As Button
+    Friend WithEvents btnEvansFundra As Button
+End Class
---
+++ Form1.vb
@@ -0,0 +1,29 @@
+﻿Public Class Form1
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+        Me.Text = "My first App in 2023"
+        Me.BackColor = Color.Red
+        Me.MaximizeBox = True
+        Me.MinimizeBox = False
+        Me.Size = New Size(800, 800)
+        Me.Opacity = 0.901    ' makes it a bit transparent
+        Me.CenterToParent()   'centers the execution box
+
+    End Sub
+
+    Private Sub btnClose_Click(sender As Object, e As EventArgs) Handles btnClose.Click
+        Close()
+
+
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles btnEvansFundra.Click
+
+
+
+        MsgBox("this is Evans",, "Greeting") 'this is to make the lil box have a Hi instead of default words
+
+
+
+    End Sub
+End Class
